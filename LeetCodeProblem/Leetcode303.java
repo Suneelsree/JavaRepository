1fc71d1afc8f9184e645c23d6ee4beb19674797b
Suneelsree <suneel28122003@gmail.com>
2026-01-02 20:41:28 +0530

    Range Sum Query - Immutable

---
+++ LeetCodeProblem/Leetcode303.java
@@ -0,0 +1,29 @@
+//Range Sum Query - Immutable
+
+public class Leetcode303 {
+    public static void main(String[] args) {
+        int nums[]={-2,0,3,-5,2,-1};
+        NumArray n=new NumArray(nums);
+        int res=n.sumRange(0, 3);
+        System.out.println(res);
+    }
+}
+class NumArray {
+    int n[];
+    public NumArray(int[] nums) {
+       n=new int[nums.length];
+       n[0]=nums[0];
+       for(int i=1;i<n.length;i++)n[i]=n[i-1]+nums[i];
+    }
+    
+    public int sumRange(int left, int right) {
+        if(left==0)return n[right];
+        return n[right]-n[left-1];
+    }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+}
