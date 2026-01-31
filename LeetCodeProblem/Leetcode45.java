a22b69751ab708403b00a80ea511a7bea8b35e60
Suneelsree <suneel28122003@gmail.com>
2026-01-31 11:52:41 +0530

    45. Jump Game II hooks law

---
+++ LeetCodeProblem/Leetcode45.java
@@ -0,0 +1,30 @@
+//hooks law
+
+import java.util.Arrays;
+
+public class Leetcode45 {
+
+    public static void main(String[] args) {
+        Leetcode45 l=new Leetcode45();
+        int num[]={2,3,1,1,4};
+        int res=l.jump(num);
+        System.out.println(res);
+    }
+    public int jump(int[] nums) {
+        int n=nums.length;
+       int dp[]=new int[n];
+       Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(j+nums[j]>=i && dp[j] != Integer.MAX_VALUE)
+                {
+                    dp[i]=Math.min(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
